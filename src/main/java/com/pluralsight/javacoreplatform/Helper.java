fa4616a6c1eb6b62f7009683ede4f591d10f6c67
Jeff Wong <v-jwong1@expedia.com>
2020-01-19 15:18:02 +1000

    use try with resources syntax

--- src/main/java/com/pluralsight/javacoreplatform/Helper.java
+++ src/main/java/com/pluralsight/javacoreplatform/Helper.java
@@ -1,4 +1,20 @@
 package com.pluralsight.javacoreplatform;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.io.Writer;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class Helper {
+    public static Reader openReader(String fileName) throws IOException, URISyntaxException {
+//        return Files.newBufferedReader(Paths.get(fileName));
+        return Files.newBufferedReader(Paths.get(Main.class.getResource(fileName).toURI()));
+    }
+
+    public static Writer openWriter(String fileName) throws IOException, URISyntaxException {
+//        return Files.newBufferedWriter(Paths.get(fileName));
+        return Files.newBufferedWriter(Paths.get(Main.class.getResource(fileName).toURI()));
+    }
 }
--- src/main/java/com/pluralsight/javacoreplatform/Main.java
+++ src/main/java/com/pluralsight/javacoreplatform/Main.java
@@ -1,7 +1,58 @@
 package com.pluralsight.javacoreplatform;
 
+import java.io.IOException;
+import java.io.Reader;
+import java.net.URISyntaxException;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello Java core platform!");
+//        doTryCatchFinally();
+        doTryWithResources();
+    }
+
+    public static void doTryCatchFinally() {
+        char[] buff = new char[8];
+        int length;
+        Reader reader = null;
+
+        try {
+            reader = Helper.openReader("/file1.txt");
+            while ((length = reader.read(buff)) != -1) {
+                System.out.println("\nlength: " + length);
+                for (int i = 0; i < length; i++) {
+                    System.out.println(buff[i]);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        } catch (URISyntaxException e) {
+            System.out.println("URI Syntax Exception!! - " + e.getMessage());
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e2) {
+                System.out.println(e2.getClass().getSimpleName() + " - " + e2.getMessage());
+            }
+        }
+    }
+
+    public static void doTryWithResources() {
+        char[] buff = new char[8];
+        int length;
+
+        try (Reader reader = Helper.openReader("/file1.txt")) {
+            while ((length = reader.read(buff)) != -1) {
+                System.out.println("\nlength: " + length);
+                for (int i = 0; i < length; i++) {
+                    System.out.println(buff[i]);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        } catch (URISyntaxException e) {
+            System.out.println("URI Syntax Exception!! - " + e.getMessage());
+        }
     }
 }
