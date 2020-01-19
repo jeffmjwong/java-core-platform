23767d87e6ad924ee1f53b581e9052fdbc6488e4
Jeff Wong <v-jwong1@expedia.com>
2020-01-19 19:17:52 +1000

    add only one value to StringJoiner

--- src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
+++ src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
@@ -4,9 +4,9 @@ import java.util.StringJoiner;
 
 public class StringFormatting {
     public static void main(String[] args) {
-        StringJoiner sj = new StringJoiner(", ", "3 types of rays - ", " hmmm");
-
-        sj.add("alpha");
+        StringJoiner sj = new StringJoiner(", ");
+        sj.setEmptyValue("EMPTY");
+        sj.add("");
 
         String result = sj.toString();
 
