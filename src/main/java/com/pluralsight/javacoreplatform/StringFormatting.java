dbea53ed46efc568eab84e475b885e209b1f7c5c
Jeff Wong <v-jwong1@expedia.com>
2020-01-20 18:48:26 +1000

    use %.nf formatter to specify number of decimal points

--- src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
+++ src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
@@ -4,8 +4,8 @@ import java.util.StringJoiner;
 
 public class StringFormatting {
     public static void main(String[] args) {
-        int david = 13, dawson = 11, dillon = 4, gordon = 2;
-        String s1 = String.format("My nephews are %d, %d, %d and %d years old", david, dawson, dillon, gordon);
+        double david = 13, dawson = 11, dillon = 4, gordon = 2;
+        String s1 = String.format("My nephews are %.2f, %.2f, %.2f and %.2f years old", david, dawson, dillon, gordon);
         System.out.println(s1);
     }
 }
