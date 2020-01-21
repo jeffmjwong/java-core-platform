4d33bd5af415f0e44530051f78310b6fd69f375e
Jeff Wong <v-jwong1@expedia.com>
2020-01-21 18:25:11 +1000

    use comma for integer group separator

--- src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
+++ src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
@@ -8,8 +8,8 @@ public class StringFormatting {
 //        String s1 = String.format("My nephews are %#X, %#X, %#X and %#X years old", david, dawson, dillon, gordon);
 //        System.out.println(s1);
 
-        String s1 = String.format("W:%-4d X:%-4d", 5, 235);
-        String s2 = String.format("Y:%-4d Z:%-4d", 481, 12);
+        String s1 = String.format("W:%d X:%,d", 513123, 235123133);
+        String s2 = String.format("Y:%d Z:%d", 481, 12);
         System.out.println(s1);
         System.out.println(s2);
     }
