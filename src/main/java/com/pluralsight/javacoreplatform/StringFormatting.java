dd5cf5222ea204dcd8a213b6a3599e1bbd026a75
Jeff Wong <v-jwong1@expedia.com>
2020-01-21 18:20:54 +1000

    fill empty space with 0 with the 0 flag

--- src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
+++ src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
@@ -8,8 +8,8 @@ public class StringFormatting {
 //        String s1 = String.format("My nephews are %#X, %#X, %#X and %#X years old", david, dawson, dillon, gordon);
 //        System.out.println(s1);
 
-        String s1 = String.format("W:%4d X:%4d", 5, 235);
-        String s2 = String.format("Y:%4d Z:%4d", 481, 12);
+        String s1 = String.format("W:%04d X:%04d", 5, 235);
+        String s2 = String.format("Y:%04d Z:%04d", 481, 12);
         System.out.println(s1);
         System.out.println(s2);
     }
