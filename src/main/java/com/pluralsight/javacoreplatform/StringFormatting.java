65dd94598cac4693ceb064b1a3b0a336ceb41594
Jeff Wong <v-jwong1@expedia.com>
2020-01-21 20:05:14 +1000

    use < symbol flag to specify value that was previously used

--- src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
+++ src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
@@ -4,13 +4,7 @@ import java.util.StringJoiner;
 
 public class StringFormatting {
     public static void main(String[] args) {
-//        int david = 13, dawson = 11, dillon = 4, gordon = 2;
-//        String s1 = String.format("My nephews are %#X, %#X, %#X and %#X years old", david, dawson, dillon, gordon);
-//        System.out.println(s1);
-
-        String s1 = String.format("W:% (d X:% (d", 123, -456);
-        String s2 = String.format("Y:%d Z:%d", 481, 12);
+        String s1 = String.format("%2$d %<04d %1$d", 100, 200, 300);
         System.out.println(s1);
-        System.out.println(s2);
     }
 }
