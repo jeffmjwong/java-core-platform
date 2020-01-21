0a67de0ac6b91e49b4aeab20dbeed287d27044f7
Jeff Wong <v-jwong1@expedia.com>
2020-01-21 18:17:10 +1000

    specify width flag

--- src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
+++ src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
@@ -4,8 +4,13 @@ import java.util.StringJoiner;
 
 public class StringFormatting {
     public static void main(String[] args) {
-        double david = 13, dawson = 11, dillon = 4, gordon = 2;
-        String s1 = String.format("My nephews are %.2f, %.2f, %.2f and %.2f years old", david, dawson, dillon, gordon);
+//        int david = 13, dawson = 11, dillon = 4, gordon = 2;
+//        String s1 = String.format("My nephews are %#X, %#X, %#X and %#X years old", david, dawson, dillon, gordon);
+//        System.out.println(s1);
+
+        String s1 = String.format("W:%4d X:%4d", 5, 235);
+        String s2 = String.format("Y:%4d Z:%4d", 481, 12);
         System.out.println(s1);
+        System.out.println(s2);
     }
 }
