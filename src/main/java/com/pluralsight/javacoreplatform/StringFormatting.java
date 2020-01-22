53c7eb904ae2184bd1ab421f7ca773e8f370fd45
Jeff Wong <v-jwong1@expedia.com>
2020-01-22 17:41:35 +1000

    use Formatter to write formatted strings directly to output writer stream

--- src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
+++ src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
@@ -1,10 +1,27 @@
 package com.pluralsight.javacoreplatform;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Formatter;
 import java.util.StringJoiner;
 
 public class StringFormatting {
     public static void main(String[] args) {
-        String s1 = String.format("%2$d %<04d %1$d", 100, 200, 300);
-        System.out.println(s1);
+        try {
+            doWrite(10, 8, 5, 3, 6.5);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void doWrite(int david, int dawson, int dillon, int gordon, double avgDiff) throws IOException {
+        BufferedWriter writer = Files.newBufferedWriter(Paths.get("myFile.txt"));
+
+        try (Formatter f = new Formatter(writer)) {
+            f.format("My nephews are %d, %d, %d and %d years old", david, dawson, dillon, gordon);
+            f.format("The average age between each is %.1f years", avgDiff);
+        }
     }
 }
