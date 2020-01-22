ded51ff55cc4c80c2bb146f47f07c76c0459c393
Jeff Wong <v-jwong1@expedia.com>
2020-01-22 19:23:16 +1000

    use String.split and String.matches with regex

--- src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
+++ src/main/java/com/pluralsight/javacoreplatform/StringFormatting.java
@@ -4,15 +4,21 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Formatter;
+import java.util.List;
 import java.util.StringJoiner;
 
 public class StringFormatting {
     public static void main(String[] args) {
 //        doWrite(10, 8, 5, 3, 6.5);
         String s1 = "apple, apple and orange please";
-        String s2 = s1.replaceAll("ple\\b", "ricot");
-        System.out.println(s2);
+        List<String> strings = Arrays.asList(s1.split("\\b"));
+        for (String thePart : strings) {
+            if (thePart.matches("apple")) {
+                System.out.println(thePart);
+            }
+        }
     }
 
     private static void doWrite(int david, int dawson, int dillon, int gordon, double avgDiff) {
