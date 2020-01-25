57f15050dbf89bef59ac5367597e1a7908030fbf
Jeff Wong <v-jwong1@expedia.com>
2020-01-25 21:15:18 +1000

    use SortedMap interface with TreeMap implementation and test for sorted behaviour

--- src/main/java/com/pluralsight/javacoreplatform/Collections.java
+++ src/main/java/com/pluralsight/javacoreplatform/Collections.java
@@ -3,29 +3,33 @@ package com.pluralsight.javacoreplatform;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class Collections {
     public static void main(String[] args) {
-        Set<MyClass> tree = new TreeSet<>(new MyComparator());
+//        Set<MyClass> tree = new TreeSet<>(new MyComparator());
+////
+////        tree.add(new MyClass("bbb", "b"));
+////        tree.add(new MyClass("aaa", "d"));
+////        tree.add(new MyClass("ddd", "a"));
+////        tree.add(new MyClass("ggg", "c"));
+////        tree.add(new MyClass("eee", "e"));
+////        tree.add(new MyClass("ccc", "g"));
+////        tree.add(new MyClass("fff", "f"));
+////
+////        tree.forEach(System.out::println);
 
-        tree.add(new MyClass("bbb", "b"));
-        tree.add(new MyClass("aaa", "d"));
-        tree.add(new MyClass("ddd", "a"));
-        tree.add(new MyClass("ggg", "c"));
-        tree.add(new MyClass("eee", "e"));
-        tree.add(new MyClass("ccc", "g"));
-        tree.add(new MyClass("fff", "f"));
+        SortedMap<Integer, String> map = new TreeMap<>();
 
-        tree.forEach(System.out::println);
+        map.put(68192, "ghi");
+        map.put(25511, "abc");
+        map.put(92210, "ghi");
+        map.put(15592, "def");
+        map.put(51288, "ghi");
+        map.put(39912, "ghi");
 
-        Map<String, String> map = new HashMap<>();
-
-        map.put("111", "abc");
-        map.put("222", "def");
-        map.put("333", "ghi");
-
-        map.replaceAll((k, v) -> v.toUpperCase() + k);
         map.forEach((k, v) -> System.out.println(k + " | " + v));
     }
 }
