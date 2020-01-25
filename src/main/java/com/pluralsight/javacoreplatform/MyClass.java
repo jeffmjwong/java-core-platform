56b25be13fbad0113ea8cf702427c5deb5af9b31
Jeff Wong <v-jwong1@expedia.com>
2020-01-25 20:34:42 +1000

    create some methods and override methods for MyClass

--- src/main/java/com/pluralsight/javacoreplatform/MyClass.java
+++ src/main/java/com/pluralsight/javacoreplatform/MyClass.java
@@ -1,4 +1,29 @@
 package com.pluralsight.javacoreplatform;
 
 public class MyClass {
+    private String label;
+    private String value;
+
+    public String getLabel() {
+        return this.label;
+    }
+    public String getValue() {
+        return this.value;
+    }
+
+    public MyClass(String label, String value) {
+        this.label = label;
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return label + " | " + value;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        MyClass other = (MyClass) obj;
+        return this.getValue().equalsIgnoreCase(other.getValue());
+    }
 }
