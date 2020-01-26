316e645dbe9fa3412113c21efac70d32ea0230c6
Jeff Wong <v-jwong1@expedia.com>
2020-01-26 15:05:01 +1000

    handle command line arguments that get passed in when starting java app

--- src/main/java/com/pluralsight/javacoreplatform/CommandLineArgs.java
+++ src/main/java/com/pluralsight/javacoreplatform/CommandLineArgs.java
@@ -1,4 +1,13 @@
 package com.pluralsight.javacoreplatform;
 
 public class CommandLineArgs {
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Hello Java no arguments!");
+        } else {
+            for (String word : args) {
+                System.out.println(word);
+            }
+        }
+    }
 }
