d4ad7419cf2526cda17502a54b1de940b2f258b9
Jeff Wong <v-jwong1@expedia.com>
2020-01-26 17:33:41 +1000

    load and read properties from store

--- src/main/java/com/pluralsight/javacoreplatform/CommandLineArgs.java
+++ src/main/java/com/pluralsight/javacoreplatform/CommandLineArgs.java
@@ -1,6 +1,7 @@
 package com.pluralsight.javacoreplatform;
 
 import java.io.BufferedReader;
+import java.io.Reader;
 import java.io.Writer;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -21,15 +22,32 @@ public class CommandLineArgs {
 //
 //        showFileLines(filename);
 
-        Properties props = new Properties();
-        props.setProperty("displayName", "Jim Wilson");
-        props.setProperty("accountNumber", "123-45-6789");
+//        Properties props = new Properties();
+//        props.setProperty("displayName", "Jim Wilson");
+//        props.setProperty("accountNumber", "123-45-6789");
+//
+//        try (Writer writer = Files.newBufferedWriter(Paths.get("xyz.properties"))) {
+//            props.store(writer, "My comment");
+//        } catch (Exception e) {
+//            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+//        }
 
-        try (Writer writer = Files.newBufferedWriter(Paths.get("xyz.properties"))) {
-            props.store(writer, "My comment");
+        Properties props = new Properties();
+
+        try (Reader reader = Files.newBufferedReader(Paths.get("myapp.properties"))) {
+            props.load(reader);
         } catch (Exception e) {
             System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
         }
+
+        String val1 = props.getProperty("val1");
+        System.out.println(val1);
+        String val2 = props.getProperty("val2");
+        System.out.println(val2);
+        String val3 = props.getProperty("val3");
+        System.out.println(val3);
+        String val4 = props.getProperty("val4");
+        System.out.println(val4);
     }
 
     private static void showFileLines(String filename) {
