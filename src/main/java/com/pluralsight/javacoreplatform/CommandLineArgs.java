e775794b71228ed76d439fc740b5a9e20b364559
Jeff Wong <v-jwong1@expedia.com>
2020-01-26 15:27:24 +1000

    finish up showFileLines method

--- src/main/java/com/pluralsight/javacoreplatform/CommandLineArgs.java
+++ src/main/java/com/pluralsight/javacoreplatform/CommandLineArgs.java
@@ -1,5 +1,9 @@
 package com.pluralsight.javacoreplatform;
 
+import java.io.BufferedReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class CommandLineArgs {
     public static void main(String[] args) {
         if (args.length < 1) {
@@ -13,5 +17,15 @@ public class CommandLineArgs {
 
     private static void showFileLines(String filename) {
         System.out.println();
+
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(filename))) {
+            String line = null;
+
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            System.out.println(e.getClass().getSimpleName() + " - " + e.getMessage());
+        }
     }
 }
