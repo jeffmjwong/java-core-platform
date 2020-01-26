41289f50ecc4a305748c643b0d03cb2c0d8ce497
Jeff Wong <v-jwong1@expedia.com>
2020-01-26 18:48:25 +1000

    create MyDefaultValues.xml file

--- src/main/java/com/pluralsight/javacoreplatform/JavaProperties.java
+++ src/main/java/com/pluralsight/javacoreplatform/JavaProperties.java
@@ -1,4 +1,32 @@
 package com.pluralsight.javacoreplatform;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
+
 public class JavaProperties {
+    public static void main(String[] args) {
+        try {
+
+            Properties userProps = new Properties();
+            loadUserProps(userProps);
+        } catch (IOException e) {
+            System.out.println("Exception <" + e.getClass().getSimpleName() + ">" + e.getMessage());
+        }
+    }
+
+    private static Properties loadUserProps(Properties userProps) throws IOException {
+        Path userFile = Paths.get("userValues.xml");
+
+        if (Files.exists(userFile)) {
+            try (InputStream inputStream = Files.newInputStream(userFile)) {
+                userProps.loadFromXML(inputStream);
+            }
+        }
+
+        return userProps;
+    }
 }
