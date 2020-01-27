73c8d56dd1c20a1624e756f6fbdf29dfbb8e0f55
Jeff Wong <v-jwong1@expedia.com>
2020-01-27 16:34:42 +1000

    create instance of logger from LoggerManager and log some info level messages

--- src/main/java/com/pluralsight/javacoreplatform/JavaLogging.java
+++ src/main/java/com/pluralsight/javacoreplatform/JavaLogging.java
@@ -1,4 +1,16 @@
 package com.pluralsight.javacoreplatform;
 
+import java.util.logging.Level;
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
 public class JavaLogging {
+    public static void main(String[] args) {
+        LogManager lm = LogManager.getLogManager();
+
+        Logger logger = lm.getLogger(Logger.GLOBAL_LOGGER_NAME);
+
+        logger.log(Level.INFO, "My first log message");
+        logger.log(Level.INFO, "Another message");
+    }
 }
