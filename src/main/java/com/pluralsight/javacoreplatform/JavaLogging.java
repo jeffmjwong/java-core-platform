4d71463d33da3211dc8992ad38507c878aa93fb7
Jeff Wong <v-jwong1@expedia.com>
2020-01-27 17:17:07 +1000

    create handler and formatter and set up com.pluralsight logger

--- src/main/java/com/pluralsight/javacoreplatform/JavaLogging.java
+++ src/main/java/com/pluralsight/javacoreplatform/JavaLogging.java
@@ -1,14 +1,20 @@
 package com.pluralsight.javacoreplatform;
 
 import java.util.logging.ConsoleHandler;
+import java.util.logging.Formatter;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 public class JavaLogging {
     static Logger logger = Logger.getLogger("com.pluralsight");
 
     public static void main(String[] args) {
         Handler h = new ConsoleHandler();
+        Formatter f = new SimpleFormatter();
+
+        h.setFormatter(f);
+        logger.addHandler(h);
     }
 }
