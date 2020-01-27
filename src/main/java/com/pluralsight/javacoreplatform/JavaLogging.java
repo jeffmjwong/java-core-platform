47c148a7f6f0ba17bff943c6e76fb2912d86792a
Jeff Wong <v-jwong1@expedia.com>
2020-01-27 17:15:29 +1000

    create com.pluralsight logger

--- src/main/java/com/pluralsight/javacoreplatform/JavaLogging.java
+++ src/main/java/com/pluralsight/javacoreplatform/JavaLogging.java
@@ -1,18 +1,14 @@
 package com.pluralsight.javacoreplatform;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Handler;
 import java.util.logging.Level;
-import java.util.logging.LogManager;
 import java.util.logging.Logger;
 
 public class JavaLogging {
-    static Logger logger = LogManager.getLogManager().getLogger(Logger.GLOBAL_LOGGER_NAME);
+    static Logger logger = Logger.getLogger("com.pluralsight");
 
     public static void main(String[] args) {
-        logger.setLevel(Level.INFO);
-
-        logger.log(Level.SEVERE, "Uh Oh!!");
-        logger.log(Level.INFO, "Just so you know");
-        logger.log(Level.FINE, "Hey developer dude");
-        logger.log(Level.FINEST, "You're special");
+        Handler h = new ConsoleHandler();
     }
 }
